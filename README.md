d960f0fde6457c0d875be8dc0c0d3b996f8af39b
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-04-29 18:16:12 +0100

    Parameterise things

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Attempts to find a self-enumerating pangram.
 
 ```
 echo 'find_a_solution' | irb -I"lib" -r ./lib/pangram
+
+# or
+
+echo 'find_a_solution(depth = 250000, prune_above = 2000) | irb -I"lib" -r ./lib/pangram
 ```
--- lib/pangram/pangram.rb
+++ lib/pangram/pangram.rb
@@ -1,13 +1,13 @@
-def find_a_solution
+def find_a_solution(depth = 150000, prune_above = 1000)
   add_to_pool(seed, error(seed), 0)
   while !solved? do
-    change_sentence_every(250000)
+    change_sentence_every(depth)
     current_attempt = remove_first_from_pool
     next_attempts = increments(current_attempt, language_significance)
     next_attempts.each_with_index do |attempt, index|
       add_to_pool(attempt, error(attempt), index)
     end
-    prune_pool(1000)
+    prune_pool(prune_above)
     show_progress
   end
   alert
