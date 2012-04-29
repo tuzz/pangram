a4288ff3c1afef514d1f25f2a7c5e51070c7317d
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-04-29 02:42:24 +0100

    Try more sentences, but in less detail

--- lib/pangram/pangram.rb
+++ lib/pangram/pangram.rb
@@ -1,7 +1,7 @@
 def find_a_solution
   add_to_pool(seed, error(seed), 0)
   while !solved? do
-    change_sentence_every(1000000)
+    change_sentence_every(250000)
     current_attempt = remove_first_from_pool
     next_attempts = increments(current_attempt, language_significance)
     next_attempts.each_with_index do |attempt, index|
